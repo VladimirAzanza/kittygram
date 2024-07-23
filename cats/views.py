caf29ec0dfa3849606d32686f0c918ce0cd68c25
Vladimir Azanza <vladimirazanza@gmail.com>
2024-07-23 13:44:31 +0300

    Add commentaries explaining how to add low and high level CBV for DRF

--- cats/views.py
+++ cats/views.py
@@ -17,3 +17,48 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+# If we want a low level CBV for DRF. Do not forget to change the url.:
+
+'''
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''
+
+# If we want a high level CBV for DRF. Do not forget about url.
+
+'''
+from rest_framework import generics
+
+from .models import Cat
+from .serializers import CatSerializer
+
+# GET + POST:
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+# GET + PUT + PATCH + DELETE:
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+'''
